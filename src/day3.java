7c04b8ce5c507fdac308ba1c842ae5bea1ca572e
Hari <harir25ai@vetias.ac.in>
2026-06-08 10:42:12 +0530

    numerical values

---
+++ src/day3.java
@@ -0,0 +1,12 @@
+void main()
+{
+    int n = 9;
+    for(int row = 1;row <= n;row++)
+    {
+        for(int col = 1;col <= row;col++)
+        {
+            System.out.print(col);
+        }
+        System.out.println();
+    }
+}
