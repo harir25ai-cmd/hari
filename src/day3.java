07f5fd7a9b2a0ac5ab32330cc33c4bf53ed76263
Hari <harir25ai@vetias.ac.in>
2026-06-08 11:46:07 +0530

    reversed loop

--- src/day3.java
+++ src/day3.java
@@ -1,9 +1,9 @@
 void main()
 {
-    int n = 9;
+    int n = 5;
     for(int row = 1;row <= n;row++)
     {
-        for(int col = 1;col <= row;col++)
+        for(int col = n;col > row-1;col--)
         {
             System.out.print(col);
         }
