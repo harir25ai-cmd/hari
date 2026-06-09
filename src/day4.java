fd6bf4af0ef1902908a3976484ba299d8ce1b691
Hari <harir25ai@vetias.ac.in>
2026-06-09 12:00:39 +0530

    2d array initilation

--- src/day4.java
+++ src/day4.java
@@ -1,3 +1,25 @@
 void main() {
-
+     int[][] attendence = new int[][]{
+             {0, 1, 0},
+             {1, 0, 0},
+             {1, 1, 1},
+             {1, 0, 1},
+             {0, 0, 0}
+     };
+     System.out.println("Output :");
+     for (int i = 0;i < attendence.length;i++)
+     {
+         System.out.println("student " + (i+1) +":");
+         for (int j = 0;j < attendence[i].length;j++)
+         {
+             if (attendence[i][j] == 1)
+             {
+                 System.out.println("Class"+(j+1)+": Present");
+             }
+             else
+             {
+                 System.out.println("Class"+(j+1)+": Absent");
+             }
+         }
+     }
 }
